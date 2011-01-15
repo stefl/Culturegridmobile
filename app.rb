a5c67ab6e30c8a427f7700d3332bc3089ede4d11
steflewandowski <stef@stef.io>
2011-01-15 14:06:11 +0000

    cross domain probs

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
-require 'sinatra'
+$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..","gem","lib"))) # for local development
 
-set :public, File.dirname(__FILE__)
+require 'rubygems'
+require 'sinatra'
+require "sinatra/jsonp"
+require "json"
+require "culturegrid"
+
+set :public, File.dirname(__FILE__) + '/public'
 
 get '/' do
   send_file 'index.html'
+end
+
+get '/search' do
+  response.headers['Cache-Control'] = "public, max-age=#{60 * 60 * 24}"
+  content_type :json
+  query = params[:q]
+  opts = params.delete_if{|k,v| k.to_s == "q" }
+  puts "query: #{query}, params: #{params.inspect}"
+  jsonp CultureGrid.search(query, params)
 end
